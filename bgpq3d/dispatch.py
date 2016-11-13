4680625b3e6dbb7c33f4751d3779c32e0aa2b4d8
Ben Maddison <benm@workonline.co.za>
2016-11-13 15:20:18 +0200

    added dispatcher init args for api use

--- bgpq3d/dispatch.py
+++ bgpq3d/dispatch.py
@@ -8,27 +8,34 @@ except ImportError:
     can_daemonize = False
 
 class Dispatcher(object):
-    def __init__(self, args=None, test=False):
-        if not isinstance(args, Namespace):
+    def __init__(self, args=None, test=False, config_path=None,
+                 host=None, port=None, bgpq3_path=None, output_class=None, object=None):
+        if args and not isinstance(args, Namespace):
             raise TypeError("%s not of type %s" % (args, Namespace))
-        config_path = args.config_path or None
+        config_path = config_path or args.config_path or None
         self._config = configuration.Config(config_path=config_path)
-        self._host = args.host or self.config.get("host")
-        self._port = args.port or self.config.get("port")
-        self._path = self.config.get("bgpq3_path")
+        self._host = host or args.host or self.config.get("host")
+        self._port = port or args.port or self.config.get("port")
+        self._path = bgpq3_path or self.config.get("bgpq3_path")
         if not args.dummy:
             self._bgpq3 = bgpq3.Bgpq3(host=self.host, port=self.port, path=self.path)
         else:
             self._bgpq3 = bgpq3.Dummy(host=self.host, port=self.port, path=self.path)
         self._output_class_name = args.output or self.config.get("output_class_name") or "JsonOutput"
         self._output_class = output.TestOutput if test else getattr(output, self.output_class_name)
-        if args.object:
-            self._object = args.object
-            self.dispatch = self.one_shot
+        self._object = args.object
 
-    def one_shot(self):
+    def dispatch(self):
         data = self.bgpq3.pl(self.object)
         return self._output_class(data=data).output()
+
+    @property
+    def object(self):
+        return self._object or None
+
+    @object.setter
+    def object(self, name=None):
+        self._object = name
 
     @property
     def config(self):
@@ -49,10 +56,6 @@ class Dispatcher(object):
     @property
     def bgpq3(self):
         return self._bgpq3
-
-    @property
-    def object(self):
-        return self._object or None
 
     @property
     def output_class_name(self):
